3426c453b9358fdc4a8545fa48d846ce845602dc
Filipe Campos <filipepcampos24@gmail.com>
2024-07-21 23:43:43 +0200

    add BRAX dataset

--- medical_diffusion/data/datasets/dataset_simple_2d.py
+++ medical_diffusion/data/datasets/dataset_simple_2d.py
@@ -1,8 +1,10 @@
+import os
 import torch.utils.data as data
 import torch
 from torch import nn
 from pathlib import Path
 from torchvision import transforms as T
+import numpy as np
 import pandas as pd
 import math
 
@@ -317,6 +319,78 @@ class MIMIC_CXR_Dataset(SimpleDataset2D):
             weights[index] = weight_per_class[target]
         return weights
 
+class Brax(SimpleDataset2D):
+    attr_names = ["Cardiomegaly"]
+    prefix = "files/brax/1.1.0/"
+
+    def __init__(self, split="train", data_filter=None, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        assert split in ["train", "valid", "test"]
+
+        df = self._maybe_process(data_filter)
+        # Split data into train, validation and test
+        train_df = df.sample(frac=0.8, random_state=0)
+        val_df = df.drop(train_df.index)
+        test_df = val_df.sample(frac=0.8, random_state=0)
+        val_df = val_df.drop(test_df.index)
+        self.data = (
+            val_df if split == "valid" else train_df if split == "train" else test_df
+        )
+
+        # store index of the selected attributes in the columns of the data for faster indexing
+        self.attr_idxs = [self.data.columns.tolist().index(a) for a in self.attr_names]
+
+    def __getitem__(self, idx):
+        # 1. select and load image
+        img_path = self.data.iloc[idx, 2]  # 'Path' column is 2
+        img = Image.open(os.path.join(self.path_root, self.prefix, img_path)).convert("RGB")
+        if self.transform is not None:
+            img = self.transform(img)
+
+        # 2. select attributes as targets
+        attr = self.data.iloc[idx, self.attr_idxs].values.astype(np.float32)
+        attr = torch.from_numpy(attr).item()
+
+        # 3. save index for extracting the patient_id in prediction/eval results as 'CheXpert-v1.0-small/valid/patient64541/study1'
+        #    performed using the extract_patient_ids function
+        idx = self.data.index[
+            idx
+        ]  # idx is based on len(self.data); if we are taking a subset of the data, idx will be relative to len(subset);
+        # self.data.index(idx) pulls the index in the original dataframe and not the subset
+
+        return {"source": self.transform(img), "target": attr}
+
+    def __len__(self):
+        return len(self.data)
+
+    def _maybe_process(self, data_filter):
+        # Dataset labels are: blank for unmentioned, 0 for negative, -1 for uncertain, and 1 for positive.
+        # Process by:
+        #    1. fill NAs (blanks for unmentioned) as 0 (negatives)
+        #    2. fill -1 as 1 (U-Ones method described in paper)  # TODO -- setup options for uncertain labels
+        #    3. apply attr filters as a dictionary {data_attribute: value_to_keep} e.g. {'Frontal/Lateral': 'Frontal'}
+        return self._load_and_preprocess_training_data(
+            os.path.join(self.path_root, "master_spreadsheet_update.csv"),
+            data_filter,
+        )
+
+    def _load_and_preprocess_training_data(self, csv_path, data_filter):
+        train_df = pd.read_csv(csv_path, keep_default_na=True)
+
+        # 1. fill NAs (blanks for unmentioned) as 0 (negatives)
+        # attr columns ['No Finding', ..., 'Support Devices']; note AP/PA remains with NAs for Lateral pictures
+        train_df[self.attr_names] = train_df[self.attr_names].fillna(0)
+
+        # 2. fill -1 as 1 (U-Ones method described in paper)  # TODO -- setup options for uncertain labels
+        train_df[self.attr_names] = train_df[self.attr_names].replace(-1, 1)
+
+        if data_filter is not None:
+            # 3. apply attr filters
+            # only keep data matching the attribute e.g. df['Frontal/Lateral']=='Frontal'
+            for k, v in data_filter.items():
+                train_df = train_df[train_df[k] == v]
+
+        return train_df
 
 class MIMIC_CXR_ImageFolder(data.Dataset):
     def __init__(self, split_path, split="train", *args, **kwargs):
